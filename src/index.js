cf716f4c0914c87d79460c3f7b2f66ae7be85ce5
Yosyp Dobosh <joseph.dobosh@gmail.com>
2023-01-26 14:02:25 -0500

    complete setup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import configureStore from './store';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 
+
+const store = configureStore();
+
 function Root() {
   return (
+    <Provider sotre={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
+    </Provider>
   );
+}
+
+if (process.env.NODE_ENV !== "production") {
+  window.store = store;
 }
 
 ReactDOM.render(
